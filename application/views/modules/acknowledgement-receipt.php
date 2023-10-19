e0190b92c8a92caa5e52f46eada44c72591cab44
ngs-jimbs <143084439+ngs-jimbs@users.noreply.github.com>
2023-10-19 17:29:27 +0800

    update pdf print of ack. receipt

--- application/views/modules/acknowledgement-receipt.php
+++ application/views/modules/acknowledgement-receipt.php
@@ -1,13 +1,5 @@
-<?php $_data = $data;
-function byDate($obj)
-{
-    return $obj["date_time"] == 1;
-}
-;
-
-?>
 <div class="row">
-    <div class="col-md-12 grid-margin stretch-card">
+    <div class="col-md-12 grid-margin stretch-card" id="toPrint">
         <div class="card">
             <div class="card-body">
                 <div class="d-sm-flex align-items-center mb-4">
@@ -23,11 +15,6 @@ function byDate($obj)
                     <div class="col-md-2">
                         <label for="end_date">End Date:</label>
                     </div>
-                    <!-- <div class="col-md-2 ">
-                        <label>&nbsp;</label>
-                        <button class="btn-outline-info  border-0 btn-sm h-25"
-                            onclick="applyDateFilter()">Select</button>
-                    </div> -->
                 </div>
                 <div class="row mb-3 ml-auto">
                     <div class="col-md-2">
@@ -63,7 +50,7 @@ function byDate($obj)
                     <tbody>
                         <?php
                         // Assuming $data is your array of data
-                        foreach ($_data as $row) {
+                        foreach ($data as $row) {
                             $date = date_create($row['date_time']);
                             echo "<tr>";
                             echo "<td>" . $row['id'] . "</td>";
@@ -77,7 +64,7 @@ function byDate($obj)
                             echo "<td>&#8369; " . $row["tax"] . "</td>";
                             echo "<td>&#8369; " . $row["total_amount"] . "</td>";
                             echo "<td>" . $row["reference_number"] . "</td>";
-                            echo "<td><button type='button' class='btn-sm btn-outline-info btn-print-receipt' data-receipt-id='" . $row['id'] . "'  onclick='printRow(" . json_encode($row) . ")'>Download</button></td>";
+                            echo "<td><button type='button' class='btn-sm btn-outline-info btn-print-receipt' data-receipt-id='" . $row['id'] . "'  onclick='printRow(" . $row['id'] . ")'>Download</button></td>";
                             echo "</tr>";
                         }
                         ?>
@@ -85,6 +72,9 @@ function byDate($obj)
                     </tbody>
 
                 </table>
+                <div class='row divider'>
+                    <div class='col-12 my-5 py-2 border'></div>
+                </div>
             </div>
 
         </div>
@@ -102,17 +92,39 @@ function byDate($obj)
         });
     });
 
-    async function printRow(rowData) {
+    const _jsonData = JSON.parse('<?php echo json_encode($data) ?>')
 
+    async function printRow(id) {
+        const rowData = _jsonData.find(obj => obj.id == id)
+        let doc = new jspdf.jsPDF({
+            orientation: 'p', unit: 'px'
+        })
+
+        let printContent = `
+            <html><title>Receipt</title>
+                <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"
+                integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous">
+                <\/script>
+                <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
+                integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous"><\/link></head><body>
+                [content]
+                </html>
+                `;
+        let content = ""
         try {
-            let printContent = `
-                    <div class=" mx-auto mt-5" style="width: 50rem">
+
+
+            //             ${i % 2 == 1 ? '<div class="row" style="width: 52vw; border-bottom:1px black solid; border-bottom-style: dashed; margin: 6rem 0 6.18rem 0"></div>' : ''}
+            // ${i % 2 == 1 && i != 39 ? "margin-bottom:12rem !important;" : ""}
+
+            content += `
+                <div class="mx-auto my-5" style="width: 50rem; ">
                     <div class="text-center my-3">Letterhead of the Intermediary</div>
                     <div class="border border-dark">
                     <div class="text-center text-uppercase py-3">
-                        <u>Acknowledgement Receipt</u>
-                    </div>`;
-            printContent += ` <div class="row d-flex">
+                        <u>Acknowledgement &nbsp; Receipt</u>  
+                </div>
+                <div class="row d-flex">
                     <div class="col"></div>
                     <div class="col">
                         <div class="row d-flex">
@@ -136,7 +148,7 @@ function byDate($obj)
             for (const key in rowData) {
                 if (["id", "ar_number", "date_time"].includes(key)) continue
 
-                printContent += `
+                content += `
                         <div class="row d-flex">
                             <div class="col text-capitalize">${key.split("_").join(" ")}<div class="float-right">:</div>
                             </div>
@@ -144,40 +156,54 @@ function byDate($obj)
                                 ${rowData[key]}
                             </div>
                     </div>
-            `
+                `;
 
             }
 
-            printContent += ` </div>
-            </div>
-
-                <div class="row mb-4">
-                    <span class="col font-italic font-weight-light">This is a system generated receipt. Signature is not required.</span>
+            content += ` </div>
                 </div>
-            </div>`;
+                    <div class="row mb-4">
+                        <span class="col font-italic font-weight-light">This is a system generated receipt. Signature is not required.</span>
+                    </div>
+                </div>`;
 
-            printContent += `<div class="row">
+            content += `<div class="row">
                     <div class="col-1">Note:</div>
                     <div class="col">
                         <div>This proforma represents minimum data.</div>
                         <div>Moreover, the format may vary depending on the system being used</div>
                     </div>
-                </div></div>`
-
-            let doc = window.open("", "My Page", "height=700,width=900,titlebar=no,resizable=no")
-            doc.document.write(`<html><title>Receipt</title>
-         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"
-        integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous">
-        <\/script>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
-        integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous"><\/link></head><body>`);
-            doc.document.write(printContent);
-            doc.document.write("</html>")
-            doc.document.close();
-            doc.document.onkeydown = (key) => alert(key)
-            setTimeout(() => { doc.print(); doc.close(); }, 100)
+                </div></div>
+                `;
         } catch (e) { console.log(e) }
+        //     let doc = window.open("", "My Page", "height=700,width=900,titlebar=no,resizable=no")
+        //     doc.document.write(`<html><title>Receipt</title>
+        //  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"
+        // integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous">
+        // <\/script>
+        // <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
+        // integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous"><\/link></head><body>`);
+        //     doc.document.write(printContent);
+        //     doc.document.write("</html>")
+        //     doc.document.close();
+        //     doc.document.onkeydown = (key) => alert(key)
+        //     setTimeout(() => { doc.print(); doc.close(); }, 300)
+
+        doc.html(printContent.replace("[content]", content), {
+            html2canvas: {
+                scale: .5
+            },
+            callback: async function (doc) {
+                // doc.addPage(
+                //     { orientation: 'p', unit: 'px' }
+                // )
+                await doc.output("dataurlnewwindow", "receipt.pdf");
+            },
+            x: 25,
+            y: 10,
+        })
     }
+
 
     function applyDateFilter() {
         var start_date = $('#start_date').val();
