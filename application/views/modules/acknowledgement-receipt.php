6c9bbf780fd0080caf3df667a8738d78bcdf7919
SheanRu <109832452+SheanRu@users.noreply.github.com>
2024-01-16 14:36:53 +0800

    acknowledgement
    fixing table

--- application/views/modules/acknowledgement-receipt.php
+++ application/views/modules/acknowledgement-receipt.php
@@ -58,7 +58,7 @@
                 <div class="col row d-flex justify-content-end py-2">
                     <div class="card-body">
                         <div class="d-sm-flex align-items-center mb-4">
-                            <h4 class="card-title mb-sm-0">Acknowledge Reciept</h4>
+                            <!--     -->
                         </div>
                     </div>
                     <div class=" col-mb-3 mr-3 mt-3">
@@ -169,11 +169,11 @@
 
             <div class="overflow-hidden border-top border-bottom border-black">
                 <div class="top-btn-container row mb-1 py-2 px-1">
-                    <div class="col-md-3 mb-2">
+                    <div class="col-md-2 mb-2">
                         <label for="start_date">Start Date:</label>
                         <input type="date" id="start_date" class="form-control">
                     </div>
-                    <div class="col-md-3 mb-2">
+                    <div class="col-md-2 mb-2">
                         <label for="end_date">End Date:</label>
                         <input type="date" id="end_date" class="form-control">
 
@@ -221,7 +221,7 @@
                             echo "<td>&#8369; " . number_format((float) $row["tax"], 2, '.', '') . "</td>";
                             echo "<td>&#8369; " . number_format((float) $row["total_amount"], 2, '.', '') . "</td>";
                             echo "<td>" . $row["reference_number"] . "</td>";
-                            echo "<td><button type='button'style='width: 100px; height: 34px;' class=' btn-outline-info btn-print-receipt' data-receipt-id='" . $row['id'] . "'  onclick='printRow(" . $row['id'] . ")'>Download</button></td>";
+                            echo "<td><button type='button'style='width: 80px; height: 25px;' class=' btn-outline-info btn-print-receipt' data-receipt-id='" . $row['id'] . "'  onclick='printRow(" . $row['id'] . ")'>Download</button></td>";
                             echo "</tr>";
                         }
                         ?>
@@ -763,7 +763,7 @@
                 content += `
                 ${i % 3 !== 0 ? '<div class="row" style="width: 89.5vw; border-bottom:1px black solid; border-bottom-style: dashed; margin: 6rem 0 6rem 0"></div>' : ''}
                 <div id="ECollectionForm-${i}" class="bg-whites mx-auto mt-5" style="width: 90rem;${(i + 1) % 3 !== 0 || i == 0 ? "" : "margin-bottom: 10rem !important;margin-top: 50rem !important;"} }">
-                <div class="container "style="margin-top: 20rem !important;">
+                <div class="container "style="margin-top: 10rem !important;">
         <div class="row">
             <div class="col text-center">
                 <span class="fs-3 font-weight-bold">REPORT OF e-COLLECTION AND DEPOSIT</span>
@@ -970,6 +970,8 @@
 
 
     </div>
+    <div class="container"style="margin-bottom:25rem !important;"">
+    </div>
 
                 </div>
             `;
@@ -987,15 +989,13 @@
         let pdfHeight = 1200; // Adjust this value based on your content
         doc.html(printContent.replace("[content]", content).replaceAll("[total-page]", 1).replaceAll("[total-amount]", totalAmount.toFixed(2)), {
             html2canvas: {
-                scale: .3
+                scale: .31
             },
             callback: async function (doc) {
                 await doc.output("dataurlnewwindow", "e-collectionReport.pdf");
             },
             x: 15,
             y: 15,
-            margin: 10,
-            height: pdfHeight
         });
     }
 
