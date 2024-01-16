b012245dde405cf6dd0f984891c9421f1a837dcb
SheanRu <109832452+SheanRu@users.noreply.github.com>
2024-01-16 14:37:46 +0800

    deposit
    button for download

--- application/views/modules/deposit.php
+++ application/views/modules/deposit.php
@@ -20,62 +20,63 @@
         <div class="card">
             <div class="card-body p-1 pt-3">
                 <div class="d-sm-flex align-items-center my-4 pl-2">
-                    <h4 class="card-title mb-sm-0">Deposit</h4>
+                    <!-- <h4 class="card-title mb-sm-0">Deposit</h4> -->
+                    <button type="button"
+                        class="btn-sm btn-outline-dark mr-3 mb-2 rounded download-btn-modal">Deposit</button>
                 </div>
 
 
-                    <table id="myTable" class="table table-striped text-center ecollection-table-container"
-                        width="100%">
-                        <thead class="w-100">
-                            <tr>
-                                <th colspan="2">Electronic Acknowledgement Receipt</th>
-                                <th rowspan="3">Responsibility Center Code</th>
-                                <th rowspan="3">Payor</th>
-                                <th rowspan="3">Particulars</th>
-                                <th rowspan="3">PREXC/PAP</th>
-                                <th colspan="3">Amount</th>
-                            </tr>
-                            <tr>
-                                <th rowspan="2">Date</th>
-                                <th rowspan="2">Number</th>
-                                <th rowspan="2">Total per AR</th>
-                                <th colspan="3">Breakdown Collection</th>
-                            </tr>
-                            <tr>
-                                <th>Taxes</th>
-                                <th colspan="1">Fees</th>
-                            </tr>
-                        </thead>
-                        <tbody class="w-100">
+                <table id="myTable" class="table table-striped text-center ecollection-table-container" width="100%">
+                    <thead class="w-100">
+                        <tr>
+                            <th colspan="2">Electronic Acknowledgement Receipt</th>
+                            <th rowspan="3">Responsibility Center Code</th>
+                            <th rowspan="3">Payor</th>
+                            <th rowspan="3">Particulars</th>
+                            <th rowspan="3">PREXC/PAP</th>
+                            <th colspan="3">Amount</th>
+                        </tr>
+                        <tr>
+                            <th rowspan="2">Date</th>
+                            <th rowspan="2">Number</th>
+                            <th rowspan="2">Total per AR</th>
+                            <th colspan="3">Breakdown Collection</th>
+                        </tr>
+                        <tr>
+                            <th>Taxes</th>
+                            <th colspan="1">Fees</th>
+                        </tr>
+                    </thead>
+                    <tbody class="w-100">
 
-                            <?php
-                            // usort($data, function ($a, $b) {
-                            //     return strtotime($a['date_time']) < strtotime($b['date_time']);
-                            // });
+                        <?php
+                        // usort($data, function ($a, $b) {
+                        //     return strtotime($a['date_time']) < strtotime($b['date_time']);
+                        // });
+                        
+                        foreach ($data as $row) {
+                            echo "<tr>";
+                            echo "<td>" . $row["date_time"] . "</td>";
+                            echo "<td>" . $row["ar_number"] . "</td>";
+                            echo "<td>" . $row["agency_name"] . "</td>";
+                            echo "<td>" . $row["name_of_payor"] . "</td>";
+                            echo "<td>" . $row["particulars"] . "</td>";
+                            echo "<td>&#8369; " . number_format((float) $row["amount"], 2, '.', '') . "</td>";
+                            echo "<td>&#8369; " . number_format((float) $row["service_charge"], 2, '.', '') . "</td>";
+                            echo "<td>&#8369; " . number_format((float) $row["tax"], 2, '.', '') . "</td>";
+                            echo "<td>&#8369; " . number_format((float) $row["total_amount"], 2, '.', '') . "</td>";
+                            echo "</tr>";
+                        }
+                        ?>
 
-                            foreach ($data as $row) {
-                                echo "<tr>";
-                                echo "<td>" . $row["date_time"] . "</td>";
-                                echo "<td>" . $row["ar_number"] . "</td>";
-                                echo "<td>" . $row["agency_name"] . "</td>";
-                                echo "<td>" . $row["name_of_payor"] . "</td>";
-                                echo "<td>" . $row["particulars"] . "</td>";
-                                echo "<td>&#8369; " . number_format((float) $row["amount"], 2, '.', '') . "</td>";
-                                echo "<td>&#8369; " . number_format((float) $row["service_charge"], 2, '.', '') . "</td>";
-                                echo "<td>&#8369; " . number_format((float) $row["tax"], 2, '.', '') . "</td>";
-                                echo "<td>&#8369; " . number_format((float) $row["total_amount"], 2, '.', '') . "</td>";
-                                echo "</tr>";
-                            }
-                            ?>
-
-                        </tbody>
-                    </table>
-                </div>
+                    </tbody>
+                </table>
             </div>
-
-
         </div>
+
+
     </div>
+</div>
 </div>
 </div>
 <script>
