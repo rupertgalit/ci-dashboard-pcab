bca88da6a1d41a99c331669273712d358c158354
michael angelo garcia <garciacruzmichaelangelo@gmail.com>
2024-02-02 16:13:22 +0800

    fix select date background

--- application/views/modules/acknowledgement-receipt.php
+++ application/views/modules/acknowledgement-receipt.php
@@ -305,7 +305,7 @@
             <div class="form-group">
                 <label for="startDate" class="date-label">Start Date:</label>
                 <div class="input-group date date-input-group" id="startDatePicker">
-                    <input type="text" class="form-control" name="startDate" id="startDate" style="z-index: 2" readonly placeholder="YYYY / MM / DD">
+                    <input type="text" class="form-control" name="startDate" id="startDate" style="z-index: 2; background:#fff;border:1px solid black;" readonly placeholder="YYYY / MM / DD">
                     <span class="input-group-addon" id="startDateIcon">
                         <i class="glyphicon glyphicon-calendar"></i>
                     </span>
@@ -313,7 +313,7 @@
 
                 <label for="endDate" class="date-label">End Date:</label>
                 <div class="input-group date date-input-group" id="endDatePicker">
-                    <input type="text" class="form-control" name="endDate" id="endDate" readonly placeholder="YYYY / MM / DD">
+                    <input type="text" class="form-control" name="endDate" id="endDate" style="background:#fff;border:1px solid black;" readonly placeholder="YYYY / MM / DD">
                     <span class="input-group-addon" id="endDateIcon">
                         <i class="glyphicon glyphicon-calendar"></i>
                     </span>
