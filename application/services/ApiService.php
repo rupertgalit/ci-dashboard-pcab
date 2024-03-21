fa983a1e2ae622abba343c24109108dd191133b6
rhonbhorj <rhonbhorj@gmail.com>
2024-03-21 13:48:54 +0800

    adding AR_No

--- application/services/ApiService.php
+++ application/services/ApiService.php
@@ -81,26 +81,7 @@ class ApiService
     {
         $curl = curl_init();
 
-        // curl_setopt_array($curl, array(
-        //     CURLOPT_URL => $endpoint,
-        //     CURLOPT_RETURNTRANSFER => true,
-        //     CURLOPT_ENCODING => '',
-        //     CURLOPT_MAXREDIRS => 10,
-        //     CURLOPT_TIMEOUT => 0,
-        //     CURLOPT_FOLLOWLOCATION => true,
-        //     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        //     CURLOPT_CUSTOMREQUEST => 'POST',
-        //     CURLOPT_POSTFIELDS => json_encode($data),
-        //     CURLOPT_HTTPHEADER => array(
-        //         'Content-Type: application/json'
-        //     )
-        // ));
-
-        // $response = curl_exec($curl);
-        // $http_status_code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-        // curl_close($curl);
-
-
+  
 
         curl_setopt_array($curl, array(
             CURLOPT_URL => $endpoint,
@@ -117,8 +98,9 @@ class ApiService
             ),
           ));
           
-          $response = curl_exec($curl);
- // $http_status_code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+  $response = curl_exec($curl);
+        $http_status_code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        curl_close($curl);
         $resp['response'] = $response;
         $resp['status_code'] = $http_status_code;
 
